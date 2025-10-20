b517aee11b7060cc1ae8e718f7d4ff1a51cdd4b1
Goodman <evelyn.goodman@442-drvemp7lptp.aruplab.net>
2025-10-19 20:11:48 -0400

    added template format for components

---
+++ app/component-template/page.tsx
@@ -0,0 +1,11 @@
+// pages/component-template.tsx
+import ComponentName from "@/components/ComponentTemplate";
+
+export default function ComponentNamePage() {
+    return (
+        <ComponentName
+            title="Component Name"
+            description="Component desciption goes here."
+        />
+    );
+}
